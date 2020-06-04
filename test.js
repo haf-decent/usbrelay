e3df786b559bbe5b3e85c6247b693e54e96f8a01
Ryan Hafner <r.hafner20@gmail.com>
2020-06-04 19:50:56 -0400

    clean up code a bit, fix one typo in USBrelay

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const {USBrelay, Board} = require('./index.js');
+const { USBrelay, Board } = require('./index.js');
 
-var ports = ['/dev/test1', '/dev/test2', '/dev/test3'];
+var ports = [ '/dev/test1', '/dev/test2', '/dev/test3' ];
 var group = new USBrelay({
     test: true,
     ports: ports
+});
+
+var board1 = new Board({
+    test: true,
+    port: ports[0]
 });
 
 //group.toggleOne(18, "on", (err) => {
@@ -13,22 +18,22 @@ var group = new USBrelay({
 //    else console.log(group.getStates());
 //});
 
-group.toggle([1,5,10,11,19,26,28,33,47], "on", (errors, success) => {
+group.toggle([ 1,5,10,11,19,26,28,33,47 ], "on", (errors, success) => {
     if (errors) console.log(errors);
     else console.log(group.getStates());
 });
 
-//group.toggle([[1,5,10,11],[3,10,12],[1,15]], "on", (errors, success) => {
+//group.toggle([ [ 1,5,10,11 ],[ 3,10,12 ],[ 1,15 ] ], "on", (errors, success) => {
 //    if (errors) console.log(errors);
 //    else console.log(group.getStates());
 //});
 
-//group.setStates([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],[],[]], (errors, success) => {
+//group.setStates([ [ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 ],[],[] ], (errors, success) => {
 //    if (errors) return console.log(errors);
 //    console.log(group.getStates());
 //});
 
-//var states = Array.from({length: 48}, (el, i) => i%2);
+//var states = Array.from({ length: 48 }, (_, i) => i % 2);
 //group.setStates(states, (errors, success) => {
 //    if (errors) return console.log(errors);
 //    console.log(group.getStates());
